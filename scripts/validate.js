fea34ad8e7ec7d66403586dfbcb8cb691a1d376d
Evgeniy Makin <rwer9982@yandex.ru>
2022-08-20 19:24:22 +0300

    17v

---
+++ scripts/validate.js
@@ -0,0 +1,54 @@
+const validationForm = {
+    formSelector: '.edit-form',
+    inputSelector: '.edit-form__input',
+    submitButtonSelector: '.edit-form__submit-button',
+    inactiveButtonClass: 'edit-form__submit-button_disabled',
+    inputErrorClass: 'edit-form__input_with-error',
+    errorClass: 'edit-form__span',
+};
+
+function showInputError(formElement, inputElement, errorMessage, form) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(form.inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(form.errorClass);
+
+}
+
+function hideInputError(formElement, inputElement, form) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(form.inputErrorClass);
+    errorElement.classList.remove(form.errorClass);
+    errorElement.textContent = '';
+
+}
+
+function checkInputValidity(formElement, inputElement, form) {
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage, form);
+    } else {
+        hideInputError(formElement, inputElement, form);
+    }
+};
+
+
+function setEventListeners(formElement, form) {
+    const inputList = Array.from(formElement.querySelectorAll(form.inputSelector));
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', function () {
+            formElement.addEventListener('submit', (evt) => {
+                evt.preventDefault();
+            });
+            checkInputValidity(formElement, inputElement, form);
+        });
+    });
+};
+
+function enableValidation(form) {
+    const formList = Array.from(document.querySelectorAll(form.formSelector));
+    formList.forEach((formElement) => {
+        setEventListeners(formElement, form);
+    });
+}
+
+enableValidation(validationForm);
